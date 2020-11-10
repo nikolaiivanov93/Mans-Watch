8c9d287178a168b10c9e4e2f1aaf20c54430fb41
nikolaiivanov93 <72261815+nikolaiivanov93@users.noreply.github.com>
2020-11-10 16:35:32 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+//scroll
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
+
+
+//hamburger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__list'),
+    //menuList = document.querySelector('.menu__list'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu__list_active');
+        });
+    });
+});
